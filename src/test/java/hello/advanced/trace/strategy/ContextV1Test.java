d514fbb6ace2e4c81f3607349270ec2fdf555ed0
hong2wrd <hong2wrd@gmail.com>
2023-02-17 17:01:55 +0900

    전략 패턴 - 예제1

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogin1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogin2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -33,6 +36,21 @@ public class ContextV1Test {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogin1 strategyLogin1 = new StrategyLogin1();
+        ContextV1 context1 = new ContextV1(strategyLogin1);
+        context1.execute();
+
+        StrategyLogin2 strategyLogin2 = new StrategyLogin2();
+        ContextV1 context2 = new ContextV1(strategyLogin2);
+        context2.execute();
     }
 
 }
