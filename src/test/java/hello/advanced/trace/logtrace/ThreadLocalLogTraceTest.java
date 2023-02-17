7c5ded21c88196234f8c1d52e2094ae988cf0e4c
hong2wrd <hong2wrd@gmail.com>
2023-02-17 13:34:42 +0900

    쓰레드 로컬 동기화 - 개발

--- src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
+++ src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -1,12 +1,11 @@
-package hello.advanced.logtrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import org.junit.jupiter.api.Test;
 
-public class FieldLogTraceTest {
+public class ThreadLocalLogTraceTest {
 
-    FieldLogTrace trace = new FieldLogTrace();
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
 
     @Test
     void begin_end_level2() {
