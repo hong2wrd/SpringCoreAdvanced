ad53e6d7d8b14cd5dd5c0c70a9ceb36e56b00ac7
hong2wrd <hong2wrd@gmail.com>
2023-02-20 14:45:06 +0900

    람다 표현식

--- src/main/java/me/whiteship/java8to11/Foo.java
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -42,5 +42,53 @@ public class Foo {
         UnaryOperator<Integer> plus12 = (i) -> i +10; //입력값과 결과값을 타입이 같을 경우에 사용(UnaryOperator)
         System.out.println(plus11.apply(1));
 
+
+        //람다 표현식
+        Supplier<Integer> get11 = () -> 11;
+        Supplier<Integer> get12 = () -> {
+            return 12;
+        };
+
+        BiFunction<Integer, Integer, Integer> sum1 = (a, b) -> a + b;
+        BinaryOperator<Integer> sum2 = (a, b) -> a + b;
+        BinaryOperator<Integer> sum3 = (Integer a, Integer b) -> a + b;
+
+        Foo foo = new Foo();
+        foo.run();
+
+    }
+
+    private void run() {
+        int baseNumber = 10; // 사실상 final 선언됨
+
+        // Local class start
+        class LocalClass {
+            int baseNumber = 11;
+            void printBaseNumber() {
+                System.out.println(baseNumber); // 11
+            }
+        }
+        // Local class end
+
+        // Anonymous class start
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer + baseNumber);
+            }
+        };
+        // Anonymous class end
+
+        /**
+         * Local, Anonymous 는 변수가 쉐도잉(Shadowing)이 됨
+         * Lambda 는 쉐도잉이 일어나지 않음(같은 스코프이기 때문에)
+         */
+
+        // Lambda
+        Consumer<Integer> printInt1 = (i) -> System.out.println(i);
+        IntConsumer printInt2 = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+        printInt2.accept(10);
     }
 }
