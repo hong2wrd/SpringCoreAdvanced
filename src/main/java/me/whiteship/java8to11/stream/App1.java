538af0017cbdb3b60646acd6dbbf9f5b50847222
hong2wrd <hong2wrd@gmail.com>
2023-02-21 14:51:09 +0900

    Stream API

---
+++ src/main/java/me/whiteship/java8to11/stream/App1.java
@@ -0,0 +1,74 @@
+package me.whiteship.java8to11.stream;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App1 {
+    public static void main(String[] args) {
+        List<OnlineClass1> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass1(1, "spring boot", true));
+        springClasses.add(new OnlineClass1(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass1(3, "spring mvc", false));
+        springClasses.add(new OnlineClass1(4, "spring core", false));
+        springClasses.add(new OnlineClass1(5, "rest api development", false));
+
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("close 되지 않은 수업");
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass1::isClosed))
+                .forEach(oc -> System.out.println(oc.getId()));
+//                .filter(oc -> !oc.isClosed())
+//                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+                .map(oc -> oc.getTitle())
+                .forEach(System.out::println);
+
+        List<OnlineClass1> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass1(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass1(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass1(6, "The Java 8 to 11", false));
+
+        List<List<OnlineClass1>> hongEvents = new ArrayList<>();
+        hongEvents.add(springClasses);
+        hongEvents.add(javaClasses);
+
+        System.out.println("두 수업 목록에 들어 있는 모든 수업 아이디 출력");
+        hongEvents.stream()
+                .flatMap(Collection::stream)
+                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10, i -> i +1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("자바 수업 중에 Test가 들어 있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        springClasses.stream()
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(OnlineClass1::getTitle)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        springClasses.stream()
+                .map(oc -> oc.getTitle())
+                .filter(t -> t.contains("spring"))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+}
