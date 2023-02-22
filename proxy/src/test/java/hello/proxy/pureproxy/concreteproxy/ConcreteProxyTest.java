bb7767069fef0fd9d768385c1eb2b95bd4110e45
hong2wrd <hong2wrd@gmail.com>
2023-02-22 17:05:34 +0900

    구체 클라스 기반 프록시 - 예제2

--- proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogin;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogin concreteLogin = new ConcreteLogin();
         ConcreteClient client = new ConcreteClient(concreteLogin);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogin concreteLogin = new ConcreteLogin();
+        TimeProxy timeProxy = new TimeProxy(concreteLogin);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
---
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,27 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeProxy extends ConcreteLogin {
+
+    private ConcreteLogin concreteLogin;
+
+    public TimeProxy(ConcreteLogin concreteLogin) {
+        this.concreteLogin = concreteLogin;
+    }
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+
+        String result = concreteLogin.operation();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeDecorator 종료 resultTime={}ms", resultTime);
+        return result;
+    }
+}
