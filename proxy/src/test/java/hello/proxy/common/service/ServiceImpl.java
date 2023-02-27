07b27ea96b585a20cc5d940e10b3e3073949b8e5
TAEHONG <hong2wrd@gmail.com>
2023-02-27 10:07:08 +0900

    오타 수정

--- proxy/src/test/java/hello/proxy/common/service/ServiceImpl.java
+++ proxy/src/test/java/hello/proxy/common/service/ServiceImpl.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ServiceImpl implements ServiceInterface{
     @Override
     public void save() {
-        log.info("sava 호출");
+        log.info("save 호출");
     }
 
     @Override
