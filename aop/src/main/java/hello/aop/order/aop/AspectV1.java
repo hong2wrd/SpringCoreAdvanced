e79cd1187952e4bec60197091e70e1e39b17525f
TAEHONG <hong2wrd@gmail.com>
2023-03-03 09:11:11 +0900

    스프링 AOP 구현1 - 시작(주석추가)

--- aop/src/main/java/hello/aop/order/aop/AspectV1.java
+++ aop/src/main/java/hello/aop/order/aop/AspectV1.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class AspectV1 {
 
+    //hello.aop.order 패키지와 하위 패키지
     @Around("execution(* hello.aop.order..*(..))") // @Around 는 pointcut
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{ // doLog 는 Advice
         log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
